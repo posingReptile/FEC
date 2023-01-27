dddd9d8bb725401bf465c12513fa2b76f376ee2b
ctunakan <clairetunakan@gmail.com>
2023-01-27 15:44:06 -0600

    Rating BD Skeleton complete

--- client/src/components/RateAndReviewFolder/RatingBreakdown.jsx
+++ client/src/components/RateAndReviewFolder/RatingBreakdown.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 const RatingBreakdown = (props) => {
   return (
-    <div>
+    <div data-testid='rating-breakdown'>
       <h3>Rating Breakdown</h3>
+      <div data-testid='rating-bd-sum'>
+        <h5>Rating Summary</h5>
+        <h5>{/*Placeholder for product avg rating*/}4.5</h5>
+        <h5>{/*Placeholder for stars*/}stars</h5>
+      </div>
+      <div data-testid='rating-bd-rec'>
+        <p>% of reviewers recommend this product</p>
+      </div>
+      <div data-testid='rating-bd-stars'>
+        <a>5 Stars</a> <br/>
+        <a>4 Stars</a> <br/>
+        <a>3 Stars</a> <br/>
+        <a>2 Stars</a> <br/>
+        <a>1 Stars</a> <br/>
+      </div>
     </div>
   )
 }
