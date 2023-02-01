3a01cac52425c7537d5d8f5f2724d78965d6a9b8
Anthony Chen <anthonychen223@gmail.com>
2023-02-01 13:55:19 -0500

    css refactor and expand view function

--- client/src/components/OverviewFolder/OverviewFooter.jsx
+++ client/src/components/OverviewFolder/OverviewFooter.jsx
@@ -5,11 +5,11 @@ const OverviewFooter = ({item}) => {
 
   return (
       <div id="overviewFooter">
-        <div>
+        <div className="productDescription">
           <h2>Product description</h2>
           <div>{item.description}</div>
         </div>
-        <div>Social Media</div>
+        <div className="socialMedia"><h2>Social Media</h2></div>
       </div>
   );
 };
