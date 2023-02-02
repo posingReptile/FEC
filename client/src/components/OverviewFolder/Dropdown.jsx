f1b7f4218319b14d987c705ec14b0db0901b1617
Anthony Chen <anthonychen223@gmail.com>
2023-02-02 17:41:05 -0500

    fix zoom bugs

--- client/src/components/OverviewFolder/Dropdown.jsx
+++ client/src/components/OverviewFolder/Dropdown.jsx
@@ -1,17 +1,30 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { IoIosArrowDown } from "react-icons/io";
 // import "./OverviewCss/dropdown.css";
 
 
 const Dropdown = ({placeHolder, sizeOption, setSizeSelector}) => {
   let [showDrop, setShowDrop] = useState(false);
+  let [selectedValue, setSelectedValue] = useState(null);
   let style = {
     visibility: showDrop ? null : 'hidden'
   }
-  // console.log(option);
+  useEffect(() => {
+    const handler = () => setShowDrop(false);
+    window.addEventListener("click", handler);
+    return () => {
+      window.removeEventListener('click', handler)
+    };
+  });
+
+  const handleInputClick = (e) => {
+    e.stopPropagation();
+    setShowDrop(!showDrop);
+  }
+
   return (
     <div className="dropdown-container" >
-      <div className="dropdown-input" onClick={()=>{setShowDrop(!showDrop)}}>
+      <div className="dropdown-input" onClick={handleInputClick}>
         <div className="dropdown-selected-value">{placeHolder}</div>
         <div className="dropdown-tools"><IoIosArrowDown /></div>
       </div>
--- client/src/components/OverviewFolder/ImageGallery.jsx
+++ client/src/components/OverviewFolder/ImageGallery.jsx
@@ -1,47 +1,58 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import { IoIosExpand } from "react-icons/io";
 import "./OverviewCss/imageGallery.css";
 import CarouselThumbnail from './CarouselThumbnail.jsx';
 
-const ImageGallery = ({photos, mainPhoto, setMainPhoto, expandView, setExpandView}) => {
+const ImageGallery = ({photos, mainPhoto, setMainPhoto, expandView, setExpandView, zoom, setZoom}) => {
+
+
 
   const mainContainer = document.getElementById("test");
   const img = document.getElementById("expandview");
+
 
   if (img && mainContainer) {
     var onZoom = useCallback(e => {
       const x = e.clientX - e.target.offsetLeft;
       const y = e.clientY - e.target.offsetTop;
-      img.style.transition = "transform 0.5s";
-      img.style.transformOrigin = `${x}px ${y}px`;
-      img.style.transform = "scale(2)";
+      mainContainer.style.transition = "transform 0.5s";
+      mainContainer.style.transformOrigin = `${x}px ${y}px`;
+      mainContainer.style.transform = "scale(2)";
     }, [])
     var offZoom = useCallback(e => {
-        img.style.transform = "scale(1)";
+      mainContainer.style.transform = "scale(1)";
     }, []);
  }
 
-  let expandFunction = (expandView) => {
-    if (!expandView) {
-      img.style.cursor = 'crosshair'
+  let expandFunction = (zoom) => {
+    if (!zoom) {
+      mainContainer.style.cursor = `url('https://img.icons8.com/material-outlined/24/null/minus-math.png'), auto`;
+      // img.style.cursor = `url('https://img.icons8.com/material-outlined/24/null/minus-math.png'), auto`;
       img.addEventListener("mousemove", onZoom);
       img.addEventListener("mouseover", onZoom);
       img.addEventListener("mouseleave", offZoom);
     } else {
-      img.style.cursor = 'zoom-in'
+      img.style.transform = "scale(1)";
+      mainContainer.style.transform = "scale(1)";
+      // img.style.cursor = 'crosshair';
+      mainContainer.style.cursor = 'crosshair';
       img.removeEventListener("mousemove", onZoom);
       img.removeEventListener("mouseover", onZoom);
       img.removeEventListener("mouseleave", offZoom);
     }
   }
 
+
+
   return (
     <div id="imageGallery">
       <div id="main">
-        <div id="test"><img id="expandview" src={mainPhoto} ></img></div>
-        <div className="expand" onClick={()=> {setExpandView(!expandView); expandFunction(expandView)}}><IoIosExpand /></div>
+        {expandView ?
+        <div id="test" onClick={() => {setZoom(!zoom); expandFunction(zoom)}} style={{cursor: 'crosshair'}}><img id="expandview" src={mainPhoto}></img></div> :
+        <div id="test" onClick={() => {setExpandView(!expandView)}} style={{cursor: 'zoom-in'}}><img id="expandview" src={mainPhoto}></img></div>}
+        <div className="expand" onClick={() => {setExpandView(!expandView)}}><IoIosExpand /></div>
       </div>
-      <CarouselThumbnail photos={photos.photos} setMainPhoto={setMainPhoto} mainPhoto={mainPhoto}/>
+      <CarouselThumbnail photos={photos.photos} setMainPhoto={setMainPhoto} mainPhoto={mainPhoto} zoom={zoom}/>
     </div>
   );
 };
