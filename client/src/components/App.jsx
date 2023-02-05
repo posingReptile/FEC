3e7f3d60fd4821bad89abb682f493d6e174382fc
Anthony Chen <anthonychen223@gmail.com>
2023-02-05 10:25:58 -0500

    fixed button showing up even when there's less photos

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,7 +9,7 @@ import RelatedProducts from './RelatedProducts/RelatedProducts.jsx';
 
 
 const App = () => {
-  const [product_id, setProductId] = useState('37318');
+  const [product_id, setProductId] = useState('37311');
   const [productRating, setProductRating] = useState(0);
   const [totalNumReviews, setTotalNumReviews] = useState(0);
 
