928de762855ddcbdb46d143bba86537a1d84e665
ctunakan <clairetunakan@gmail.com>
2023-02-07 20:50:02 -0600

    Finished New Review testing

--- client/src/components/RateAndReviewFolder/NewReview.jsx
+++ client/src/components/RateAndReviewFolder/NewReview.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import CharReview from './CharReview.jsx';
-import { Container, HorizontalContainer, HorizontalStarList, HorizontalImgList, StyledImgList,StyledReviewSummary, StyledReviewBody, RedAsterisk, NewReviewTopPortion, LeftAndRight, RadioDiv, StyledEmailInput,StyledSubmit } from '../styled/NewReview.styled.js';
+import { Container, HorizontalContainer, HorizontalStarList, HorizontalImgList, StyledImgList,StyledReviewSummary, StyledReviewBody, RedAsterisk, NewReviewTopPortion, LeftAndRight, RadioDiv, StyledEmailInput, StyledSubmit } from '../styled/NewReview.styled.js';
 import './RateAndReview.css';
 import { FaStar } from 'react-icons/fa';
 
@@ -60,7 +60,7 @@ const NewReview = ({charArray, charChoice, setCharChoice, product_id, charOption
 
   return (
     <div>
-      <Container>
+      <Container role="add-new-review">
       <h4>Write Your Review</h4>
       <h5>About the {item.name}</h5>
       <form onSubmit={handleReviewSubmit}>
@@ -99,13 +99,14 @@ const NewReview = ({charArray, charChoice, setCharChoice, product_id, charOption
             </HorizontalContainer>
           </label><br/>
             <h5>Do you recommend this product?<RedAsterisk>*</RedAsterisk></h5>
-            <RadioDiv>
+            <RadioDiv role="recommend-radio">
               {recommendOptions.map((valueObj, k) => (
                 <label key={k}>
                   <input type="radio"
                   value={valueObj.value}
                   name={valueObj.name}
                   onChange={handleRecClick}
+                  role={`rec-${valueObj.value}`}
                   checked={recommend.name === valueObj.value}
                   /><br/>{valueObj.value}
                 </label>
@@ -121,23 +122,25 @@ const NewReview = ({charArray, charChoice, setCharChoice, product_id, charOption
             charChoice={charChoice}
             setCharChoice={setCharChoice}/>
           <br />
-          <fieldset>
+          <fieldset role="summary">
             <legend><h5>Review Summary</h5></legend>
               <StyledReviewSummary name="summary"
+              role="type-summary"
               placeholder="Example: Best purchase ever!"
               maxLength="60"
               onChange={(e) => handleTextInput(e, setSummary)}></StyledReviewSummary>
           </fieldset>
-          <fieldset>
+          <fieldset role="review-body">
             <legend><h5>Review Body<RedAsterisk>*</RedAsterisk></h5></legend>
               <StyledReviewBody name="body"
               placeholder="Why did you like the product or not?"
               onChange={(e) => handleTextInput(e, setBody)}
               minLength="50"
+              role="type-body"
               maxLength="1000" required></StyledReviewBody>
           </fieldset>
-          <input type="file" id="reviewImgUpload" name="imgUpload" accept="image/png, image/jpeg" max="5" onChange={(e) => handleImageSelection(e.target)} multiple/>
-          <fieldset>
+          <input type="file" id="reviewImgUpload" name="imgUpload" accept="image/png, image/jpeg" max="5" role="handle-image-upload" onChange={(e) => handleImageSelection(e.target)} multiple/>
+          <fieldset role="photo-preview">
             <legend><h5>Photo Preview</h5></legend>
             <HorizontalImgList>
             {img.map((image, index) => (
@@ -147,7 +150,7 @@ const NewReview = ({charArray, charChoice, setCharChoice, product_id, charOption
           ))}
           </HorizontalImgList>
           </fieldset>
-        <StyledSubmit type="submit" />
+        <StyledSubmit type="submit" role="submit"/>
       </form>
       </Container>
     </div>
